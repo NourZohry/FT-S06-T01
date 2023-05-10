b1d3f4a9787b2f87a16e7c6a8150e0ea61abba2f
NourZohry <nzohry@gmail.com>
2023-05-10 14:36:01 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+let cardsWrapper = document.getElementById("cards-wrapper");
+cardsWrapper.innerHTML = `<div class="row">
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-1.png" alt="Card icon">
+    <h1>اﻟﺄﻓﻀﻞ ﻣﺸﻮرة</h1>
+    <p>اﻧﺘﻘﺎء اﻟﻤﻨﺘﺠﺎت اﻟﺄﻓﻀﻞ. اﻟﻤﺴﺘﺜﻤﺮ ﻋﻠﻰ ﺗﻜﻮﻳﻦ ﻣﺤﺎﻓﻆ ﻣﺘﻨﻮﻋﺔ وﻋﻠﻰ اﻟﻌﺪﻳﺪ ﻣﻦ اﻟﺂﻟﻴﺎت اﻟﺤﺼﺮﻳﺔ اﻟﺘﻲ ﺗﺴﺎﻋﺪ ﺑﺎﻟﺈﺿﺎﻓﺔ إﻟﻰ اﺳﺘﻘﻠﺎﻟﻴﺘﻨﺎ ﻓﻲ دراﻳﺔ، ﻓﻘﺪ ﻃﻮرﻧﺎ</p>
+  </div>
+</div>
+
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-2.png" alt="Card icon">
+    <h1>اﻟﺄﻛﺜﺮ راﺣﺔ</h1>
+    <p>ﻋﺒﺮ ﻣﻮﻗﻊ واﺣﺪ وﺣﺴﺎب واﺣﺪ. ﺑﻞ أﺻﺒﺢ ﺑﺈﻣﻜﺎﻧﻚ ﻣﺘﺎﺑﻌﺔ ﻛﻞ اﺳﺘﺜﻤﺎراﺗﻚ وإدارﺗﻬﺎ ﻓﻲ ﻛﺎﻓﺔ اﻟﻤﻨﺘﺠﺎت اﻟﺎﺳﺘﺜﻤﺎرﻳﺔ ﻣﻦ ﻣﻜﺎن واﺣﺪ، أﺳﻤﺎء دﺧﻮل ﻣﺨﺘﻠﻔﺔ ﻟﺄن دراﻳﺔ ﺗﺘﻴﺢ ﻟﻚ اﻟﺎﺳﺘﺜﻤﺎر ﻟﻦ ﺗﺤﺘﺎج إﻟﻰ ﻓﺘﺢ ﺣﺴﺎﺑﺎت ﻣﺘﻌﺪدة؛ أو ﺣﻔﻆ</p>
+  </div>
+</div>
+
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-3.png" alt="Card icon">
+    <h1>اﻟﺄﻛﺜﺮ ﺗﻨﻮﻳﻌًﺎ</h1>
+    <p>المؤشرات المتداولة FTE التداول في ما يزيد عن 002,1 من صناديق والصكوك، والمرابحة...إلخ( والأسواق كما يمكنك تستثمر في كافة الفئات الاستثمارية )كالأسهم، استثماري من 01 مدراء مختلفين. هذه المنتجات تمكنك دراية من الاستثمار في أكثر من 06 منتج</p>
+  </div>
+</div>
+
+</div>
+<div class="row">
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-4.png" alt="Card icon">
+    <h1>وﺑﻠﺎ ﺗﻜﻠﻔﺔ إﺿﺎﻓﻴﺔ</h1>
+    <p>ﻣﺒﺎﺷﺮة (أي ﺑﺪون أي رﺳﻮم إﺿﺎﻓﻴﺔ). اﻟﻤﺘﺎﺣﺔ ﻋﻨﺪ اﻟﺎﺳﺘﺜﻤﺎر ﻣﻊ اﻟﺠﻬﺔ اﻟﺎﺳﺘﺜﻤﺎرﻳﺔ ﻛﺎﻓﺔ اﻟﻤﻨﺘﺠﺎت اﻟﺎﺳﺘﺜﻤﺎرﻳﺔ ﻣﺘﺎﺣﺔ ﺑﻨﻔﺲ اﻟﺘﻜﻠﻔﺔ</p>
+  </div>
+</div>
+
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-5.png" alt="Card icon">
+    <h1>اﻟﺄﻛﺜﺮ ﺷﻔﺎﻓﻴﺔ</h1>
+    <p>ﺷﻔﺎف ﻟﺎﻧﺘﻘﺎء اﻟﺄﻓﻀﻞ ﻣﻦ ﺑﻴﻨﻬﺎ. اﻟﺘﻲ ﺗﻘﺪﻣﻬﺎ دراﻳﺔ ﻣﻦ ﺧﻠﺎل ﺣﺴﺎب ﻣﻮﺣﺪ ﺑﺸﻜﻞ أﺻﺒﺢ ﻣﻦ اﻟﺴﻬﻞ ﻋﻠﻴﻚ اﻟﻤﻘﺎرﻧﺔ ﺑﻴﻦ اﻟﻤﻨﺘﺠﺎت</p>
+  </div>
+</div>
+
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-6.png" alt="Card icon">
+    <h1>اﻟﺄﻛﺜﺮ اﺳﺘﻘﻠﺎﻟﻴﺔ</h1>
+    <p>اﻟﺎﺳﺘﻘﻠﺎﻟﻴﺔ واﻟﺤﻴﺎدﻳﺔ ﻓﻲ ﺗﻌﺎﻣﻠﻬﺎ وﻣﺸﻮرﺗﻬﺎ. ﺑﻌﻴﻨﻬﺎ، اﻟﺄﻣﺮ اﻟﺬي ﻳﻌﻄﻴﻬﺎ ﻗﺪر أﻛﺒﺮ ﻣﻦ ﺗﺤﺖ ﺳﻘﻒ واﺣﺪ ﺑﺪﻟﺄ ﻣﻦ اﻟﺘﺮﻛﻴﺰ ﻋﻠﻰ ﺑﻴﻊ ﻣﻨﺘﺠﺎت دراﻳﺔ ﺗﺮﻛﺰ ﻋﻠﻰ ﺟﻤﻊ ﻛﺎﻓﺔ اﻟﻤﻨﺘﺠﺎت اﻟﺎﺳﺘﺜﻤﺎرﻳﺔ</p>
+  </div>
+</div>`
+
+function themePurple() {
+    const collection = document.getElementsByClassName("background-color");
+
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].style.backgroundColor = "#510B76";
+    }
+}
+
+function themeGreen() {
+    const collection = document.getElementsByClassName("background-color");
+
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].style.backgroundColor = "#0B760F";
+    }
+}
